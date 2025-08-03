057a5b36861dad6c7a5c2217909185304cf28235
junghunoh <junghunoh@github.com>
2025-08-03 20:36:21 +0900

    init

--- math/script.py
+++ math/script.py
@@ -7,12 +7,15 @@ gpu=input()
 
 #model='gpt'
 #model='llama'
+#model='llama2'
 model='llama3'
 
 if model == 'gpt':
     base_model = 'EleutherAI/gpt-j-6b'
 elif model == 'llama':
     base_model = 'yahma/llama-7b-hf'
+elif model == 'llama2':
+    base_model = "meta-llama/Llama-2-7b-hf"
 elif model == 'llama3':
     base_model = 'meta-llama/Meta-Llama-3-8B'
 
@@ -21,12 +24,12 @@ bs=64
 lr = 1e-4
 init='True'
 for seed in [1,2,3]:
-    for r, ratio, scale, epoch in [(4,0,4,2),(4,0,8,2)]:
+    for r, ratio, scale, epoch in [(16,0,4,3),(16,0,8,3)]:
         for method in ['base','ours','ourssvd1']:
             alpha = int(scale * (r**0.5))
-            os.system(f'CUDA_VISIBLE_DEVICES={gpu} python finetune.py \
+            os.system(f'CUDA_VISIBLE_DEVICES={gpu} python train_math.py \
                 --model_name_or_path {base_model}\
-                --data_path ft-training_set/MetaMathQA-40K.json \
+                --data_path ft-training_set/MetaMathQA-395K.json \
                 --data_length 10000000 \
                 --bf16 True \
                 --output_dir ./trained_models/{model}_metamath_epoch{epoch}_r{r}_scale{scale}_lr{lr}_ratio{ratio}_{method}_seed{seed}/\
@@ -40,6 +43,7 @@ for seed in [1,2,3]:
                 --warmup_ratio 0.03 \
                 --logging_steps 50 \
                 --num_train_epochs {epoch} \
+                --lr_scheduler_type "cosine" \
                 --target_modules q_proj k_proj v_proj up_proj down_proj \
                 --lora_r {r}\
                 --lora_alpha {alpha}\
