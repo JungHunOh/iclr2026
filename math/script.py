fff9c22212c44212a3dad97e54e340ce550acb5e
junghunoh <junghunoh@github.com>
2025-08-11 14:32:19 +0900

    init

--- math/script.py
+++ math/script.py
@@ -5,35 +5,34 @@ print('PID:', os.getpid())
 print('enter gpu')
 gpu=input()
 
-#model='gpt'
-#model='llama'
-#model='llama2'
-model='llama3'
+#model='llama3'
+model='gemma'
 
-if model == 'gpt':
-    base_model = 'EleutherAI/gpt-j-6b'
-elif model == 'llama':
-    base_model = 'yahma/llama-7b-hf'
-elif model == 'llama2':
-    base_model = "meta-llama/Llama-2-7b-hf"
-elif model == 'llama3':
+if model == 'llama3':
     base_model = 'meta-llama/Meta-Llama-3-8B'
+    target_modules = 'q_proj k_proj v_proj up_proj down_proj'
+elif model == 'gemma':
+    base_model = 'google/gemma-7b-it'
+    target_modules = 'q_proj k_proj v_proj up_proj down_proj o_proj gate_proj'
+
 
 bs=64
 
-lr = 1e-4
+lr = 2e-4
 ratio=0
 init='True'
+max_steps=-1
+epoch=3
 for seed in [1,2,3]:
-    for r, max_steps, scale, epoch in [(16,5,4,3),(16,10,8,3)]:
-        for method in ['base','ours','ourssvd1']:
+    for r, scale in [(32,4),(64,4)]:
+        for method in ['base']:
             alpha = int(scale * (r**0.5))
             os.system(f'CUDA_VISIBLE_DEVICES={gpu} python train_math.py \
                 --model_name_or_path {base_model}\
-                --data_path ft-training_set/MetaMathQA-40K.json \
+                --data_path ft-training_set/MetaMathQA-395K.json \
                 --data_length 10000000 \
                 --bf16 True \
-                --output_dir ./trained_models/{model}_metamath_epoch{epoch}_r{r}_scale{scale}_lr{lr}_ms{max_steps}_{method}_seed{seed}/\
+                --output_dir ./trained_models/{model}_metamath_epoch{epoch}_r{r}_scale{scale}_lr{lr}_{method}_seed{seed}/\
                 --per_device_train_batch_size 4 \
                 --per_device_eval_batch_size 4 \
                 --gradient_accumulation_steps {bs//4} \
@@ -46,7 +45,7 @@ for seed in [1,2,3]:
                 --max_steps {max_steps} \
                 --num_train_epochs {epoch} \
                 --lr_scheduler_type "cosine" \
-                --target_modules q_proj k_proj v_proj up_proj down_proj \
+                --target_modules {target_modules} \
                 --lora_r {r}\
                 --lora_alpha {alpha}\
                 --seed {seed}\
